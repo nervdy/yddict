657f170283c5369065dbd1f0aefe96f830967105
nervdy <450470823@qq.com>
2018-07-02 13:30:19 +0800

    ..

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const urlencode = require('urlencode')
 const noCase = require('no-case')
 const config = require('./lib/config')
 const Parser = require('./lib/parser')
+const fs = require('fs')
+const path = require('path')
 
 let word = process.argv.slice(2).join(' ')
 if (!word) {
@@ -41,5 +43,54 @@ request(options, (error, response, body) => {
 	if (config.spinner) {
 		spinner.stop(true)
 	}
-	console.log(ColorOutput(Parser.parse(isCN, body)))
+	let output = Parser.parse(isCN, body)
+	console.log(ColorOutput(output))
+
+	if (!isCN && output) {
+		save_word(word)
+	}
 })
+
+function save_word (word) {
+	let word_path = path.join(__dirname, './data/word1.json')
+	
+	if (!fs.existsSync(word_path)) {
+		fs.appendFileSync(word_path, JSON.stringify([]))
+	}
+
+	let file = fs.readFileSync(word_path, {encoding: 'utf8'})
+	file = JSON.parse(file)
+	
+	if (file.includes(word)) return
+
+	file.push(word)
+	fs.writeFile(word_path, JSON.stringify(file), () => {
+		console.log('write success:', file.length)
+	})
+	
+}
+
+// POST  http://iwordnet.com/quiz/investigateWordList.htm
+/**
+ * avalon.post("/quiz/investigateWordList.htm", {
+                w: t.join(",")
+            })
+ */
+
+//  Accept: */*
+/**
+ * Accept: * /*
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9,zh;q=0.8,zh-CN;q=0.7
+Cache-Control: no-cache
+Connection: keep-alive
+Content-Length: 151
+Content-Type: application/x-www-form-urlencoded; charset=UTF-8
+Cookie: 
+Host: iwordnet.com
+Origin: http://iwordnet.com
+Pragma: no-cache
+Referer: http://iwordnet.com/sbook/upload.htm
+User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36
+X-Requested-With: XMLHttpRequest
+ */
